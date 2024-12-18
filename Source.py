b151f3e076a4d47780ca25b0efee9f6d47c65642
MaxJensen1 <147597786+MaxJensen1@users.noreply.github.com>
2024-12-18 12:55:48 +0100

    Added automatic username grab

--- Source.py
+++ Source.py
@@ -1,20 +1,26 @@
 from typing import TypeVar, Generic, Optional
 from List import List
 import sys, time
+import os
 
 sys.setrecursionlimit(15000) # To not cap out at the default recursion depth at 1000
 
 def main():
-    testRuns = 100
+    testRuns = 10
     totalTime = 0
+
+    computerName = os.getlogin()
+    fileLocation = f"C:\\Users\\{computerName}\\Desktop\\text.txt"
 
     for i in range(testRuns):
         list = List()
-        list.AddFromtextFile("C:\\Users\\max.jensen\\Desktop\\text.txt")
+        list.AddFromtextFile(fileLocation)
 
         startTime = time.time()
         list.MergeSort()
         endTime = time.time()
+
+        #list.PrintAll()
 
         deltaTime = endTime - startTime
         print("Sorted ", list.GetLength(), " words in ", deltaTime, " seconds")   
